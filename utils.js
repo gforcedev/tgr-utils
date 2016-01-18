c6037056af23dc02a22e3464db4f3b61f1fc5c5d
fart_sparklez <josh.lavelle1@gmail.com>
2016-01-18 13:40:12 +0000

    FIX MABY??

---
+++ utils.js
@@ -0,0 +1,106 @@
+/*read Wiki page for usage!*/
+function animationHover(element, animation){
+  element = $(element);
+  element.hover(
+    function() {
+      element.addClass('animated ' + animation);
+    },
+    function(){
+      //wait for animation to finish before removing classes
+      window.setTimeout( function(){
+        element.removeClass('animated ' + animation);
+      }, 2000);
+    }
+  );
+}
+function animationClick(element, animation){
+  element = $(element);
+  element.click(
+    function() {
+      element.addClass('animated ' + animation);
+      //wait for animation to finish before removing classes
+      window.setTimeout( function(){
+          element.removeClass('animated ' + animation);
+      }, 2000);
+    }
+  );
+}
+function exitAnimate(element, animation, reEnterAnimate) {
+  element = $(element);
+  element.click(
+    function() {
+      element.addClass('animated ' + animation);
+	//wait for animation to finish before removing classes
+	window.setTimeout( function(){
+	element.removeClass(animation);
+	if (reEnterAnimate == "none") {
+	element.addClass("hidden");
+	} else {
+	element.addClass(reEnterAnimate);
+			window.setTimeout( function(){
+			element.removeClass("animated " + reEnterAnimate);
+		}, 2000);
+          }
+      }, 2000);
+    }
+  );
+}
+function animationByOthersClick(clickedElement, animatedElement, animation){
+clickedElement = $(clickedElement);
+ animatedElement = $(animatedElement);
+  clickedElement.click(
+    function() {
+      animatedElement.addClass('animated ' + animation);
+      //wait for animation to finish before removing classes
+      window.setTimeout( function(){
+          animatedElement.removeClass('animated ' + animation);
+      }, 2000);
+    }
+  );
+}
+function animationByOthersHover(hoverElement, animatedElement, animation){
+  hoverElement = $(hoverElement);
+   animatedElement = $(animatedElement);
+    hoverElement.hover(
+    function() {
+      animatedElement.addClass('animated ' + animation);
+      //wait for animation to finish before removing classes
+      window.setTimeout( function(){
+          animatedElement.removeClass('animated ' + animation);
+      }, 2000);
+    }
+  );
+}
+function animate(element, animation) {
+	element = $(element);
+      element.addClass('animated ' + animation);
+      //wait for animation to finish before removing classes
+      window.setTimeout( function(){
+      element.removeClass('animated ' + animation);
+	  }, 2000);
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+function deleteCookie( name ) {
+  document.cookie = name + '=; expires=Fri, 20 Jan 2002 11:11:11 GMT;';
+}
+function setExpireDate(cname, date) {
+    document.cookie = cname + "=" + getCookie(cname) + "; expires=" + date;
+}
+function getQueryVariable(variable) {
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
